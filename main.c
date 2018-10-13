8c0afdb41c9df1f30ed119ed755294b1d9f0e429
Albin `FumesOver` Parou <albin.parou@epita.fr>
2018-10-13 20:40:19 +0200

    add option to specify path for image in image

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <gtk/gtk.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <unistd.h>
+
 #include "image.h"
 #include "interface.h"
 #include "matrix.h"
@@ -10,10 +12,23 @@
 
 #include "neuralNetwork/neuralNet.h"
 
-int main(void)
+int main(int argc, char** argv)
 {
     // TODO : Select file from interface file chooser
     char *path_image = "/home/rhaeven/Documents/OCR/scan.jpg";
+   
+    int opt;
+    while ((opt = getopt(argc, argv, "i:")) != -1) {
+        switch (opt) {
+            case 'i':
+                path_image = optarg;
+                break;
+            default:
+                break;
+        }
+    }
+
+
     SDL_Surface *image = IMG_Load(path_image);
 
     if (image == NULL)
